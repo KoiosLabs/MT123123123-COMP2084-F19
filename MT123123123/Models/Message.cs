1ba856a0ec88ae13345f9c104af954601decc53a
Justin Hunt <Justin.hunt@georgiancollege.ca>
2019-11-06 17:29:25 -0500

    model defined

---
+++ MT123123123/Models/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MT123123123.Models
+{
+    public class Message
+    {
+        public virtual int Id { get; set; }
+
+        [Required]
+        public virtual String Name { get; set; }
+        [Required]
+        public virtual String Msg { get; set; }
+
+        public virtual DateTime PostedAt { get; set; } = DateTime.Now;
+    }
+}
